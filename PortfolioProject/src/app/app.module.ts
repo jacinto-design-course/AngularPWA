81bf0a4268c441b5034a56639b4c7bff1ec3ca87
unknown <jacinto.design.course@gmail>
2019-08-21 16:16:11 -0400

    section 11 end

--- PortfolioProject/src/app/app.module.ts
+++ PortfolioProject/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,18 +9,30 @@ import { environment } from '../environments/environment';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGithub, faCodepen, faInstagram, faYoutube, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { faGem } from '@fortawesome/free-regular-svg-icons';
+import { faCircle, faAt, faCertificate, faTrophy, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faCodepen, faInstagram, faYoutube, faLinkedin, faMedium, faHtml5, faCss3Alt, faJsSquare, faReact, faAngular } from '@fortawesome/free-brands-svg-icons';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
 
-library.add(faGithub, faCodepen, faInstagram, faYoutube, faLinkedin, faMedium);
+library.add(faGithub, faCodepen, faInstagram, faYoutube, faLinkedin, faMedium, faHtml5, faCss3Alt, faJsSquare, faReact, faAngular, faCircle, faAt, faCertificate, faTrophy, faBars, faGem);
+
+const appRoutes: Routes = [
+  { path: 'resume', component: ResumeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    ResumeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes, 
+      { enableTracing: true } //debugging
+    ),
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
